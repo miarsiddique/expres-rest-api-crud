6ab65456faf849ca8ac5577eedac70e71cfdee9c
miarsiddique <miarsiddique@gmail.com>
2019-08-19 19:33:00 +0530

    crud completed

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var UserController = require('../app/Controllers/UserController');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/', UserController.userStore);
+router.get('/lists', UserController.usersLists);
+router.put('/:id', UserController.updateUser);
+router.delete('/:id', UserController.deleteUser);
+router.get('/:id', UserController.getUserById);
 
 module.exports = router;
