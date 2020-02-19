2220f88aa2c98fc8c651cf3427e1e2a9848c14ac
jyoti-aika <irshad.alam@logiciesolutions.co.in>
2020-02-19 19:17:56 +0530

    fixed and image upload

--- app/Controllers/ImageToVideoController.js
+++ app/Controllers/ImageToVideoController.js
@@ -1,7 +1,17 @@
+var Jimp = require('jimp');
+var fs = require('fs');
+var path = require('path');
 
 module.exports = {
 
-   imageUpload: (req, res, next) => {
+	/**
+	 * image upload using multers
+	 * @param  req
+	 * @param  res
+	 * @param  next
+	 * @return
+	 */
+    imageUpload: (req, res, next) => {
       console.log(req);
       if (!req.file) {
          res.status(500);
@@ -10,10 +20,44 @@ module.exports = {
       res.json({ fileUrl: 'http://localhost:4000/images/' + req.file.filename });
 	},
 
+	/**
+	 * test onto image
+	 * @param  req
+	 * @param  res
+	 * @param  next
+	 * @return
+	 */
 	TextOverlay: (req, res, next) => {
-      res.status(200).send(req.body.text);
+      var fileName =  'http://localhost:4000/images/' + req.file.filename;
+		var imageCaption = 'Welcome Irshad Alam 2';
+      console.log(req.body.message)
+		Jimp.read(fileName)
+			.then(function (image) {
+				var path = require('path');
+				Jimp.loadFont(Jimp.FONT_SANS_16_BLACK).then(function (font) {
+					image.print(font, 10, 10, imageCaption)
+					image.color([
+					  { apply: 'hue', params: [-90] },
+					]);
+					image.write(val = 'public/images/'+'image-' + Date.now() + '.' +'jpg');
+					res.json({
+						success: 'Image Saved Successfully!!',
+						message: 'messages',
+					});
+				});
+			})
+			.catch(function (err) {
+				console.error(err);
+			});
 	},
 
+	/**
+	 * image to video create
+	 * @param  {[type]}   req  [description]
+	 * @param  {[type]}   res  [description]
+	 * @param  {Function} next [description]
+	 * @return {[type]}        [description]
+	 */
 	ImageToVideo: (req, res, next) => {
 
 	}
--- routes/video.js
+++ routes/video.js
@@ -5,7 +5,7 @@ var upload = require('../public/js/imageMulter');
 
 /* Test Overlay On image. */
 router.post('/upload', upload.single('file'), ImageToVideoController.imageUpload);
-router.get('/text-overlay', ImageToVideoController.TextOverlay);
+router.post('/text-overlay', upload.single('file'), ImageToVideoController.TextOverlay);
 router.get('/image-to-video', ImageToVideoController.ImageToVideo);
 
 module.exports = router;
